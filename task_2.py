80e4d01897b340fc49b7d8cac7680547766491f1
vitalii.loboda <vitalii.loboda@gmail.com>
2020-12-22 15:33:21 +0200

    add tasks 1-6

---
+++ task_2.py
@@ -0,0 +1,8 @@
+def user_data(name, surname, birth_year, current_city, email, phone):
+    print(
+        f'Name - {name}, Surname - {surname}, Year of birth - {birth_year}, Current city - {current_city}, '
+        f'Email - {email}, Phone - {phone}')
+
+
+user_data(birth_year='1990', surname='Bond', name='Alex', email='alex@bond.com', current_city='Boston',
+          phone='380635498549')
