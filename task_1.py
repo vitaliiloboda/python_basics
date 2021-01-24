a667445f918a8e7e00069c847c884f6694f6a38b
vitalii.loboda <vitalii.loboda@gmail.com>
2021-01-24 18:24:05 +0200

    add tasks 1-7

--- task_1.py
+++ task_1.py
@@ -37,3 +37,33 @@ matrix2 = Matrix([[5, 7, 2], [1, 6, 7], [3, 8, 2], [6, 4, 3]])
 print(matrix1)
 print(matrix2)
 print(matrix1 + matrix2)
+
+
+######################################################2#################################################################
+
+class Matrix:
+
+    def __init__(self, my_list):
+        self.my_list = my_list
+
+    def __str__(self):
+        return '\n'.join(['\t'.join(list(map(str, i))) for i in self.my_list])
+
+    def __add__(self, other):
+        result = []
+
+        for m in range(len(self.my_list)):
+            b = [self.my_list[m][k] + other.my_list[m][k] for k in range(len(self.my_list[0]))]
+            result.append(b)
+
+        return Matrix(result)
+
+
+matrix = Matrix([[1, 2, 8], [4, 3, 1], [7, 1, 4], [1, 2, 3]])
+matrix2 = Matrix([[4, 5, 3], [6, 9, 4], [6, 8, 1], [8, 9, 0]])
+
+print(matrix)
+print()
+print(matrix2)
+print()
+print(matrix + matrix2)
